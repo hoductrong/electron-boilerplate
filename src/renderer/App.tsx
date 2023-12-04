112d03455a7049289eef26b116c01f6b00a9c485
Trong Ho <trong.hoduc@gmail.com>
2023-12-04 15:46:54 +0700

    initital commit

---
+++ src/renderer/App.tsx
@@ -0,0 +1,21 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function Hello() {
+  return (
+    <div>
+      <h1>💖 Hello World!</h1>
+      <p>Welcome to your Electron application.</p>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
